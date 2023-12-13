1524aede50301dd56aa824ad176637b15b6d3299
Shubham <shubhamkumar1801305@gmail.com>
2023-12-13 21:26:46 +0530

    Employee wage

---
+++ employeeWage.java
@@ -0,0 +1,6 @@
+// Displaying welcome to employee wage computation program on master branch
+public class task1 {
+  public static void main(String[] args) {
+    System.out.println("Welcome to Employee Wage Computation Program");
+  }
+}
