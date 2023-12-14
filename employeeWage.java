5fc93a03e5d751e349b52a0bfabdfcd527ca7a04
Shubham kumar <80197634+SHUBHAM10119@users.noreply.github.com>
2023-12-14 10:19:40 +0530

    Update employeeWage.java

--- employeeWage.java
+++ employeeWage.java
@@ -6,7 +6,7 @@ public class EmployeeWageComputation {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to Employee Wage Computation Program using OOP");
+        System.out.println("Welcome to Employee Wage Computation Program ");
         int WAGE_PER_HR =20;
         int MAX_WORKING_DAYS = 20;
         int MAX_HRS_IN_MONTH = 100;
