3059932d6f637bb6347959bf0ceca1a28b5cb8b0
Shubham kumar <80197634+SHUBHAM10119@users.noreply.github.com>
2023-12-14 10:15:41 +0530

    Update employeeWage.java

--- employeeWage.java
+++ employeeWage.java
@@ -1,6 +1,24 @@
-// Displaying welcome to employee wage computation program on master branch
-public class task1 {
-  public static void main(String[] args) {
-    System.out.println("Welcome to Employee Wage Computation Program");
-  }
+package com.company;
+
+import java.util.Random;
+
+public class EmployeeWageComputation {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Employee Wage Computation Program using OOP");
+        int IS_PRESENT = 1;
+        int WAGE_PER_HR = 20;
+        int EMP_HRS;
+        int TotalWageInDay;
+        Random num = new Random();
+        int randomCheck = num.nextInt(2);
+        if (IS_PRESENT == randomCheck)
+            EMP_HRS = 8;
+        else
+            EMP_HRS = 0;
+        TotalWageInDay = WAGE_PER_HR * EMP_HRS;
+        System.out.println("Total wage in Day : "+TotalWageInDay);
+
+    }
 }
